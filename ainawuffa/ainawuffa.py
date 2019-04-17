660bef3c991e55b03f1372103d0292b89eec5201
Pete Jemian <prjemian@gmail.com>
2019-04-17 13:56:34 -0500

    core function

---
+++ ainawuffa/ainawuffa.py
@@ -0,0 +1,17 @@
+
+#-----------------------------------------------------------------------------
+# :author:    Pete R. Jemian
+# :email:     jemian@anl.gov
+# :copyright: (c) 2019, UChicago Argonne, LLC
+#
+# Distributed under the terms of the Creative Commons Attribution 4.0 International Public License.
+#
+# The full license is in the file LICENSE.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+
+def toot():
+    print("Toot from ainawuffa!")
+
+if __name__ == "__main__":
+    toot()
